aaa953e37d087b334f6d3c807c60b0d6558f6955
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-05 13:15:29 +0200

    view.go

--- views/view.go
+++ views/view.go
@@ -4,11 +4,11 @@ import "html/template"
 
 func NewView(files ...string) *View {
 	files = append(files, "views/layouts/footer.gohtml")
-
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
+
 	return &View{
 		Template: t,
 	}
