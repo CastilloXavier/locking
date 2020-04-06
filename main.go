8f985355f9c7594ac52d9f5a28980ab3385f19f6
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-06 09:10:27 +0200

    Added a Render method to our View type to simlify view rendering

--- main.go
+++ main.go
@@ -14,17 +14,13 @@ var (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(homeView.Render(w, nil))
+
 }
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(contactView.Render(w, nil))
+
 }
 
 func main() {
@@ -35,4 +31,10 @@ func main() {
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3002", r)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
