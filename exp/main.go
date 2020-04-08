af9c0704c970a5c710300655a6d82e3e26feb7b8
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-08 15:15:01 +0200

    Learning how to insert date in sql with go

--- exp/main.go
+++ exp/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -19,6 +22,7 @@ type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+	Color string
 }
 
 func main() {
@@ -33,6 +37,32 @@ func main() {
 		panic(err)
 	}
 
-	db.DropTableIfExists(&User{})
-	//db.AutoMigrate(&User{})
+	//db.DropTableIfExists(&User{})
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	name, email, color := getInfo()
+	u := User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+
+	if err = db.Create(&u).Error; err != nil {
+		panic(err)
+	}
+}
+
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What is your email address?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What is your favourite color?")
+	color, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+	return name, email, color
 }
