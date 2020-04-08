bccde7daa64ee06c2ddfdc12f2d245a2ef36dec3
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-08 14:35:12 +0200

    Learing how to create Models with GORM to database postgresql

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,34 +15,24 @@ const (
 	dbname   = "lenslocked_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	rows, err := db.Query(`
-
-    SELECT *
-    FROM users
-    INNER JOIN orders ON users.id=orders.user_id
-  `)
-	if err != nil {
+	if err := db.DB().Ping(); err != nil {
 		panic(err)
 	}
 
-	for rows.Next() {
-		var userID, orderID, amount int
-		var email, name, description string
-		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &description); err != nil {
-			panic(err)
-		}
-		fmt.Println("userID:", userID, "name:", name, "email:", email, "orderID:", orderID, "amount:", amount, "descrtiption:", description)
-	}
-	if rows.Err() != nil {
-		panic(err)
-	}
-
+	db.DropTableIfExists(&User{})
+	//db.AutoMigrate(&User{})
 }
