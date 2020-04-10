5760027140efe165c1a3306c3c17151b4ec6a71d
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-10 23:14:52 +0200

    Hashing password when create a user user type add attribut password gorm

--- models/users.go
+++ models/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func NewUserService(connectionInfo string) (*UserService, error) {
@@ -63,6 +64,12 @@ func first(db *gorm.DB, dst interface{}) error {
 
 // Create will create the provided user and backfill data.
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -103,6 +110,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null; unique_index"`
+	Name         string
+	Email        string `gorm:"not null; unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
