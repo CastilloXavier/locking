bef944394f3972d55c7af78d7bee771fbaec7b0d
Xavier Castillo <xaviercastillo@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-10 11:49:22 +0200

    Creating the user service

--- exp/main.go
+++ exp/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"lenslocked.com/models"
 )
 
 const (
@@ -15,37 +15,29 @@ const (
 	dbname   = "lenslocked_dev"
 )
 
-type User struct {
-	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique_index"`
-	Color  string
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.DestructiveReset()
+	user, err := us.ByID(1)
+	/*db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	db.LogMode(true)
 	db.AutoMigrate(&User{}, &Order{})
-
-	var users []User
+	*/
+	/*var users []User
 	if err := db.Preload("Orders").Find(&users).Error; err != nil {
 		panic(err)
-	}
-	fmt.Println(users)
+	}*/
+	fmt.Println(user)
 	//fmt.Println(users.Orders)
 	//createOrder(db, u, 1001, "Fake Description #1")
 	//createOrder(db, u, 9999, "Fake Description #2")
@@ -68,15 +60,4 @@ func main() {
 	} else {
 		fmt.Println(u)
 	}*/
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	}).Error
-	if err != nil {
-		panic(err)
-	}
 }
--- models/users.go
+++ models/users.go
@@ -1,9 +1,61 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	//defer db.Close()
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// ErrorNotFound is returned when a resource cannot be found
+// in the database.
+var (
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+//ById will look up  by the id provided
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+//Closes the UserService database connections
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+//DestructiveReset drops the user table and rebuils edit
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
